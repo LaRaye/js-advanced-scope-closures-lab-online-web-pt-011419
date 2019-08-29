0fdd9b3030839453728e3bc0251d9df8f23a37a9
LaRaye Johnson <lnj2105@gmail.com>
2019-08-29 19:15:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function produceDrivingRange(distLimit) {
   return calculateRange(start, end) {
-      
+    let dist = Math.abs(start - end);
+    let range = Math.abs(distLimit - dist);
+    return `within range by ${range}`;
   }
 }
