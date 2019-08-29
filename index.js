762a43246c10e790297838cedd6c4ec49543c0fc
LaRaye Johnson <lnj2105@gmail.com>
2019-08-29 19:23:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function produceDrivingRange(blockRange) {
   return function(start, end) {
     let dist = Math.abs(parseInt(start) - parseInt(end));
     
-    if (dist < blockRange) {
+    if (dist < blockRange) (
       return `within range by ${blockRange - dist}`;
-    }
+    ) else (
+    
+    )
   }
 }
