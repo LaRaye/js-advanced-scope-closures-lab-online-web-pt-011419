e0d34d23bea9f624b2cd096187d044348a4e0d24
LaRaye Johnson <lnj2105@gmail.com>
2019-08-29 19:20:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function(start, end) {
-    let dist = Math.abs(start - end);
+    let dist = Math.abs(parseInt(start) - parseInt(end));
     let range = Math.abs(blockRange - dist);
     return `within range by ${range}`;
   }
